97fd5ec842b7dd1fdf60d6f7bc69f09d557e845d
moneone12 <msafone89@gmail.com>
2025-07-01 10:46:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Risk management concepts for students
+const riskConcepts = [
+    "Not wearing a helmet while biking",
+    "Sharing personal info online",
+    // [Include ALL the risk concepts from before]
+    "Not reporting bullying incidents"
+];
+
+// DOM Elements
+const bingoCard = document.getElementById('bingoCard');
+// [Include ALL the JavaScript code from the previous HTML file]
